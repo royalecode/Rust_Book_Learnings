dc3bc04a426d9e1b219b849596e5a8a29afb2ceb
Edmon Bosch <edmonbosch@gmail.com>
2023-02-03 11:26:42 +0100

    first chapter of structs in learning Rust book

---
+++ structs/src/main.rs
@@ -0,0 +1,54 @@
+//stucts
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+//tuple struct
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+//unit struct
+struct AlwaysEqual;
+
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("someusername123"),
+        email: String::from("someone@example.com"),
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+
+    let userBuild = build_user(String::from("build@example.com"), String::from("builduser"));
+
+    // let user2 = User {
+    //     active: user1.active,
+    //     username: user1.username,
+    //     email: String::from("another@example.com"),
+    //     sign_in_count: user1.sign_in_count,
+    // };
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    }; //user1 no longer usable as some variables have moved
+
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
